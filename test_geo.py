4682073b319487308193c8bd1caa00f68cf34d6a
Frangoulides <cf550@cam.ac.uk>
2022-02-03 00:53:07 +0000

    Added a function that tests stations_by_distance inside test_geo.py

--- test_geo.py
+++ test_geo.py
@@ -1,1 +1,11 @@
-print(1+2)
+from floodsystem import geo
+from floodsystem import stationdata
+
+
+def test_stations_by_distance():
+
+    station_list = stationdata.build_station_list()
+
+    list = geo.stations_by_distance(station_list, (52.2053, 0.1218))
+
+    assert(len(list) == 2165)
