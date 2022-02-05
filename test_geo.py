1e129c46cf0453398bcfe6f3079f0380d4f3ab62
wwoo122 <ww395@cam.ac.uk>
2022-02-05 19:15:03 +0000

    Task 1E: rivers_by_station_number() test implemented

--- test_geo.py
+++ test_geo.py
@@ -1,6 +1,8 @@
 from floodsystem import geo
 from floodsystem import stationdata
 from haversine import haversine
+from floodsystem.station import MonitoringStation
+
 
 def test_stations_by_distance():
     stations_list = geo.stations_by_distance(stationdata.build_station_list(), (52.2053, 0.1218))
@@ -16,3 +18,21 @@ def test_stations_within_radius():
     for station in stations_list:
         radius = haversine(station.coord, (52.2053, 0.1218))
         assert radius < 10
+
+
+def test_rivers_by_station_number():
+    # Create 100 new stations on an imaginary river called 'HopefullyNotARealRiverName'.
+    stations = stationdata.build_station_list()
+    for i in range(100):
+        s_id = "test-s-id"
+        m_id = "test-m-id"
+        label = "some station"
+        coord = (-2.0, 4.0)
+        trange = (-2.3, 3.4445)
+        river = "HopefullyNotARealRiverName"
+        town = "My Town"
+        s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+        stations.append(s)
+
+    assert ("HopefullyNotARealRiverName", 100) in geo.rivers_by_station_number(stations, 5)
+    assert len(geo.rivers_by_station_number(stations, 5)) >= 5
