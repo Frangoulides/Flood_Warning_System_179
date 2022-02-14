eb6984dc3a7779c82260a555073d91ce2335c143
Frangoulides <cf550@cam.ac.uk>
2022-02-14 11:08:59 +0000

    One minor adjustment in Task2B.py

--- Task2B.py
+++ Task2B.py
@@ -5,5 +5,5 @@ stations = stationdata.build_station_list()
 data = flood.stations_level_over_threshold(stations, 0.8)
 
 print("*** Task 1C: CUED Part IA Flood Warning System *** \n")
-for i in range(10):
-    print(data[i][0].name, data[i][1])
+for i in data:
+    print(i[0].name, i[1])
