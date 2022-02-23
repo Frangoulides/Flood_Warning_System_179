12e32d21985e2003b89ab7ff5612b9d31a59a9a1
wwoo122 <ww395@cam.ac.uk>
2022-02-23 16:39:09 +0000

    maybe working, maybe not

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,17 +1,23 @@
 import matplotlib.dates
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def polyfit(dates, levels):
+def polyfit(dates, levels, p):
     """
     Given the water level time history (dates, levels) for a station,
     Computes a least-squares fit of a polynomial of degree p to water level data.
     Returns a tuple of the polynomial object and any shift of the time (date) axis.
     """
 
-    x = list(matplotlib.dates.date2num(dates))
+    x = sorted(list(matplotlib.dates.date2num(dates)), reverse=True)
     y = levels
 
-    plt.plot(x,y,".")
+    date_shift = x[0]
+    plt.plot(x, y)
 
-    return plt.show()
+    p_coeff = np.polyfit(x - date_shift, y, p)
+
+    poly = np.poly1d(p_coeff)
+
+    return poly, date_shift
