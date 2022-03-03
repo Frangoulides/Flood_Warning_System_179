edc3bc6a7e777bf82a10dfea6b6486d51769b83e
wwoo122 <ww395@cam.ac.uk>
2022-03-03 00:10:08 +0000

    changed some more values and added a river over threshold criteria ww395

--- test_geo.py
+++ test_geo.py
@@ -4,6 +4,7 @@ from haversine import haversine
 from floodsystem.station import MonitoringStation
 
 
+
 def test_stations_by_distance():
     stations_list = geo.stations_by_distance(stationdata.build_station_list(), (52.2053, 0.1218))
 
@@ -37,3 +38,37 @@ def test_rivers_by_station_number():
 
     assert ("HopefullyNotARealRiverName", 100) in geo.rivers_by_station_number(stations, 5)
     assert len(geo.rivers_by_station_number(stations, 5)) >= 5
+
+
+def test_catchment_with_stations():
+    stations = []
+    for i in range(10):
+        s_id = "test-s-id"
+        m_id = "test-m-id"
+        label = "some station"
+        coord = (-2.0, 4.0)
+        trange = (-2.3, 3.4445)
+        catchment = 'catchment'
+        river = "HopefullyNotARealRiverName"
+        town = "My Town"
+        s = MonitoringStation(s_id, m_id, label, coord, trange, catchment, river, town)
+        stations.append(s)
+
+    assert 'catchment' in geo.catchments_with_station(stations)
+
+
+def test_stations_by_catchment():
+    stations = stationdata.build_station_list()
+    for i in range(10):
+        s_id = "test-s-id"
+        m_id = "test-m-id"
+        label = "some station"
+        coord = (-2.0, 4.0)
+        trange = (-2.3, 3.4445)
+        catchment = 'catchment'
+        river = "HopefullyNotARealRiverName"
+        town = "My Town"
+        s = MonitoringStation(s_id, m_id, label, coord, trange, catchment, river, town)
+        stations.append(s)
+
+    assert len(geo.stations_by_catchment(stations)['catchment']) == 10
